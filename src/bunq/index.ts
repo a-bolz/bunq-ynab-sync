6099929de40b90b61a18f53537c5f28523ad4297
andreas bolz <andreas@komposit.dev>
2024-04-03 21:43:04 +0200

    add dotenv

--- src/bunq/index.ts
+++ src/bunq/index.ts
@@ -1,3 +1,6 @@
-export const registerCallbacks = (): null => null
+export const registerCallbacks = (): null => {
+  console.log(fetch)
+  return null
+}
 export const deregisterCallbacks = (): null => null
 export const authenticate = (): null => null
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import dotenv from 'dotenv'
+import * as bunq from './bunq/index.js'
+
+dotenv.config()
+
 const hello = (arg: string): void => {
+  console.log(bunq)
   console.log(arg)
+  bunq.registerCallbacks()
 }
 
 hello('world')
