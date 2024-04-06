5432a2fce4f528d780074e69b2e5005c0d3f173e
andreas bolz <andreas@komposit.dev>
2024-04-06 11:28:37 +0200

    feat: making first calls to ynab

--- src/ynab/index.ts
+++ src/ynab/index.ts
@@ -1,3 +1,20 @@
-import mockRequest from '../mock_data/bunq_request_body.json' assert { type: 'json' }
+import dotenv from 'dotenv'
+import ynab from 'ynab'
+// import mockRequest from '../mock_data/bunq_request_body.json' assert { type: 'json' }
 
-console.log(mockRequest)
+dotenv.config()
+
+const getYnabApi = () => {
+  const accessToken = process.env['YNAB_ACCESS_TOKEN']
+  if (!accessToken) throw new Error('YNAB ACCESS TOKEN MUST BE PROVIDED')
+  return new ynab.API(accessToken)
+}
+
+(async function () {
+  const ynabAPI = getYnabApi()
+  const budgetsResponse = await ynabAPI.budgets.getBudgets()
+  const budgets = budgetsResponse.data.budgets
+  for (const budget of budgets) {
+    console.log(`Budget Name: ${budget.name}`)
+  }
+})()
