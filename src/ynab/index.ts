461f81833dfddfbea3e5170b56a6df13322e2a13
andreas bolz <andreas@komposit.dev>
2024-04-12 15:38:05 +0200

    clean logs

--- src/ynab/index.ts
+++ src/ynab/index.ts
@@ -14,23 +14,11 @@ export async function getCreateTransactionParams (bunqRequest: any): Promise<any
     const payment = bunqRequest?.NotificationUrl?.object?.Payment
     if (!isPayment(payment)) throw new Error('Non payment received!')
 
-    console.log('this is what the payment is', JSON.stringify(payment, null, 2))
-
     const transactionParams = await getYnabParamsFromBunqPayload(payment)
     const createTransactionResult = await createTransaction(transactionParams)
-    console.log('computed transaction params are', transactionParams)
     writeLog(JSON.stringify({ bunqRequest, createTransactionResult }, null, 2))
   } catch (error) {
     console.log('error computing ynab transaction params', error)
     writeLog(JSON.stringify({ bunqRequest, transactionParams: { error } }, null, 2))
   }
 }
-
-// for (const todo of todos.slice(0, 1)) {
-//  const payment = todo.Payment as unknown as Payment
-//  getYnabParamsFromBunqPayload(payment)
-//    .then((params) => {
-//      console.log(JSON.stringify({ payment: todo, params }, null, 2))
-//      createTransaction(params)
-//    })
-// }
