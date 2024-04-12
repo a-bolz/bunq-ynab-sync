a3d6486df5bf88aa78b5c0f2c9c450808343fba2
andreas bolz <andreas@komposit.dev>
2024-04-12 15:40:12 +0200

    cleaner logs

--- src/ynab/index.ts
+++ src/ynab/index.ts
@@ -9,13 +9,14 @@ const isPayment = (arg: any): arg is Payment => {
   return typeof arg?.id === 'number' && typeof arg?.created === 'string'
 }
 
-export async function getCreateTransactionParams (bunqRequest: any): Promise<any> {
+export async function syncTransaction (bunqRequest: any): Promise<any> {
   try {
     const payment = bunqRequest?.NotificationUrl?.object?.Payment
     if (!isPayment(payment)) throw new Error('Non payment received!')
 
     const transactionParams = await getYnabParamsFromBunqPayload(payment)
     const createTransactionResult = await createTransaction(transactionParams)
+    console.log('Succesfully created YNAB transaction', JSON.stringify(createTransactionResult, null, 2))
     writeLog(JSON.stringify({ bunqRequest, createTransactionResult }, null, 2))
   } catch (error) {
     console.log('error computing ynab transaction params', error)
