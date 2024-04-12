f8093259611cb45a76fe99a3c5c6f5770310e7a1
andreas bolz <andreas@komposit.dev>
2024-04-12 11:28:18 +0200

    test commit

--- src/ynab/index.ts
+++ src/ynab/index.ts
@@ -4,5 +4,6 @@ import { getYnabParamsFromBunqPayload } from './transaction-param-mapping.js'
 import type { Payment } from '../bunq/type.ts'
 
 export async function getCreateTransactionParams (bunqRequest: Payment): Promise<any> {
+  console.log('ding')
   return await getYnabParamsFromBunqPayload(bunqRequest)
 }
